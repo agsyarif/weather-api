4c4e213d48b4986471be30db4a5c62f0da551b7c
agus syarif <syrf553@gmail.com>
2024-06-28 09:16:50 +0700

    add: babel-cli

---
+++ dist/config/redis.config.js
@@ -0,0 +1,22 @@
+import { createClient } from 'redis';
+import dotenv from 'dotenv';
+dotenv.config();
+
+let client;
+
+const redisClient = () => {
+  if (!client) {
+    client = createClient({
+      url: `redis://${process.env.REDIS_USERNAME}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
+    });
+
+    client.on('error', err => {
+      console.error('Redis Client Error:', err);
+    });
+
+    client.connect();
+  }
+  return client;
+};
+
+export { redisClient };
